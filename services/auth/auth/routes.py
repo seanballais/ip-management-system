c8e4e1797cabc9a01a11868446850ce2a6e1d304
Sean Francis N. Ballais <sean@seanballais.com>
2025-03-27 20:00:11 +0800

    Add check to ensure that the user knows the passwords in the registration API endpoint.

--- services/auth/auth/routes.py
+++ services/auth/auth/routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 
 router: APIRouter = APIRouter()
@@ -12,5 +12,28 @@ class Registration(BaseModel):
 
 @router.put('/register')
 async def register(data: Registration):
-    print(data)
-    return {}
+    if data.password1 != data.password2:
+        raise HTTPException(
+            status_code=422,
+            detail={
+                'errors': [
+                    {
+                        'code': 'mismatched_passwords'
+                    }
+                ]
+            }
+        )
+
+    return {
+        'data': {
+            'user': {
+                'id': 'TBD ID',
+                'username': data.username,
+                'is_superuser': 'TBD superuser'
+            },
+            'authorization': {
+                'access_token': '<access_token>',
+                'refresh_token': '<refresh_token>'
+            }
+        }
+    }
