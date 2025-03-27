5495f14b08d4ec692b1c80383e005809facf780c
Sean Francis N. Ballais <sean@seanballais.com>
2025-03-27 19:05:56 +0800

    Add initial work for the registration endpoint.

--- services/auth/auth/routes.py
+++ services/auth/auth/routes.py
@@ -1,9 +1,16 @@
 from fastapi import APIRouter
-
+from pydantic import BaseModel
 
 router: APIRouter = APIRouter()
 
 
-@router.get('/')
-async def index():
-    return { 'Hello': 'World' }
+class Registration(BaseModel):
+    username: str
+    password1: str
+    password2: str
+
+
+@router.put('/register')
+async def register(data: Registration):
+    print(data)
+    return {}
