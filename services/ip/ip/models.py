4b29edaaa16d395244116eb7f657c1e38f0d905b
Sean Francis N. Ballais <sean@seanballais.com>
2025-03-30 19:15:02 +0800

    Add models for the IP service.

---
+++ services/ip/ip/models.py
@@ -0,0 +1,40 @@
+from datetime import datetime, timezone
+import typing
+
+from sqlmodel import Field, JSON, SQLModel, Relationship
+
+
+class IPAddress(SQLModel, table=True):
+    id: typing.Optional[int] = Field(default=None, primary_key=True)
+    created_on: datetime = Field(default=datetime.now(timezone.utc),
+                                 nullable=False)
+    ip_address: str = Field(index=True)
+    label: str
+    comment: typing.Optional[str]
+    recorder_id: typing.Optional[int]
+
+    events: list['IPAddressEvent'] = Relationship(back_populates='ip_address')
+
+
+class IPAddressEventType(SQLModel, table=True):
+    id: typing.Optional[int] = Field(default=None, primary_key=True)
+    name: str = Field(index=True, unique=True)
+
+    events: list['IPAddressEvent'] = Relationship(back_populates='event_type')
+
+
+class IPAddressEvent(SQLModel, table=True):
+    id: typing.Optional[int] = Field(default=None, primary_key=True)
+    recorded_on: datetime = Field(default=datetime.now(timezone.utc),
+                                  nullable=False)
+    user_id: typing.Optional[int]
+    ip_address_id: typing.Optional[int] = Field(default=None,
+                                                foreign_key='ipaddress.id')
+    event_type_id: typing.Optional[int] = Field(default=None,
+                                                foreign_key='ipaddresseventtype.id')
+    old_data: typing.Optional[str] = Field(default=None, sa_type=JSON)
+
+    ip_address: typing.Optional[IPAddress] = Relationship(
+        back_populates='events')
+    event_type: typing.Optional[IPAddressEventType] = Relationship(
+        back_populates='events')
