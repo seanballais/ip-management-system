beaedb2c914815cf841b1d8038d2979d93f4bda7
Sean Francis N. Ballais <sean@seanballais.com>
2025-04-01 02:08:06 +0800

    Add the register endpoint to the gateway service.

--- services/gateway/gateway/routes.py
+++ services/gateway/gateway/routes.py
@@ -2,10 +2,28 @@ from typing import Annotated
 
 from fastapi import APIRouter, Depends, HTTPException, Query
 import jwt
+import requests
+from requests import Response
+
+from .schema import (RegistrationData, LoginData, LogoutData, UsersData,
+                     AccessTokenValidationData, TokenRefreshData, AuditData)
 
 router: APIRouter = APIRouter()
 
+AUTH_SERVICE_URL: str = 'http://auth:8080'
+IP_SERVICE_URL: str = 'http://ip:8080'
+
 
 @router.put('/register')
-async def register() -> dict:
-    return {}
+async def register(data: RegistrationData) -> dict:
+    url: str = f'{AUTH_SERVICE_URL}/register'
+
+    request_data: dict = {
+        'username': data.username,
+        'password1': data.password1,
+        'password2': data.password2
+    }
+
+    resp: Response = requests.put(url, json=request_data)
+
+    return resp.json()
---
+++ services/gateway/gateway/schema.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel
+
+
+class RegistrationData(BaseModel):
+    username: str
+    password1: str
+    password2: str
+
+
+class LoginData(BaseModel):
+    username: str
+    password: str
+
+
+class LogoutData(BaseModel):
+    access_token: str
+    refresh_token: str
+
+
+class UsersData(BaseModel):
+    access_token: str
+
+
+class AccessTokenValidationData(BaseModel):
+    access_token: str
+
+
+class TokenRefreshData(BaseModel):
+    refresh_token: str
+
+
+class AuditData(BaseModel):
+    access_token: str
