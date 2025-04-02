58a6815020ecbf4c11d26d6fb21ab4e565f02eb1
Sean Francis N. Ballais <sean@seanballais.com>
2025-04-03 02:48:05 +0800

    Add initial work for the web app client.

---
+++ clients/web/src/pages/LoginPage.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+function LoginPage(): React.ReactNode {
+    return (
+        <>
+            <h1>Login Page</h1>
+        </>
+    );
+}
+
+export default LoginPage;
