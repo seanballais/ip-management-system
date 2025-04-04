07c513ef802c8ac86a539da528a851efb898570a
Sean Francis N. Ballais <sean@seanballais.com>
2025-04-04 20:32:31 +0800

    Add initial work for the GET function that attempts to do a token refresh if there was an access token error.

--- clients/web/src/utils/api.ts
+++ clients/web/src/utils/api.ts
@@ -20,6 +20,70 @@ interface APIError {
     code: string;
 }
 
+interface FailedJSONResponse {
+    detail: {
+        errors: Array<APIError>
+    }
+}
+
+interface TokenRefreshBodyData {
+    refresh_token: string;
+}
+
+interface TokenRefreshSuccessJSONResponse {
+    data: {
+        access_token: string,
+        refresh_token: string
+    };
+}
+
+async function getWithTokenRefresh(path: string, body: string): Promise<Response> {
+    const response: Response = await get(path, body);
+    if (response.ok) {
+        return response;
+    }
+
+    // Check first if the error is about an invalid access token.
+    const origResponse: Response = response.clone();
+    const {detail}: FailedJSONResponse = await response.json();
+    if (detail.errors[0].code === 'invalid_access_token') {
+        // Refresh tokens.
+        try {
+            await refreshTokens();
+        } catch (error: unknown) {
+            throw error as Error;
+        }
+    } else {
+        // Error is something else. Just return the response.
+        return origResponse;
+    }
+
+    // Retry the original operation.
+    return get(path, body);
+}
+
+async function refreshTokens(): Promise<void> {
+    const refreshToken: string = localStorage.getItem(REFRESH_TOKEN_STORAGE_NAME) ?? '';
+
+    const bodyData: TokenRefreshBodyData = {
+        refresh_token: refreshToken
+    };
+    const response: Response = await get('token/refresh', JSON.stringify(bodyData));
+    if (response.ok) {
+        const {data}: TokenRefreshSuccessJSONResponse = await response.json();
+        localStorage.setItem(ACCESS_TOKEN_STORAGE_NAME, data.access_token);
+        localStorage.setItem(REFRESH_TOKEN_STORAGE_NAME, data.refresh_token);
+
+        return Promise.resolve();
+    } else {
+        throw new Error('Failed to refresh tokens.');
+    }
+}
+
+async function get(path: string, body: string): Promise<Response> {
+    return fetchAPI(path, HTTPMethod.GET, body);
+}
+
 async function post(path: string, body: string): Promise<Response> {
     return fetchAPI(path, HTTPMethod.POST, body);
 }
@@ -42,6 +106,7 @@ export {
     ACCESS_TOKEN_STORAGE_NAME,
     REFRESH_TOKEN_STORAGE_NAME,
     HTTPMethod,
+    getWithTokenRefresh,
     post,
     put
 };
