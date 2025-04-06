5eb3b76380f7cf9cd8fbe046628cb36ef9aaa187
Sean Francis N. Ballais <sean@seanballais.com>
2025-04-06 12:39:50 +0800

    Dockerize web app.

--- clients/web/vite.config.ts
+++ clients/web/vite.config.ts
@@ -4,8 +4,20 @@ import hmrPublicStyles from './plugins/hmr-public-styles.ts';
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: '/',
     plugins: [
         react(),
         hmrPublicStyles()
     ],
+    preview: {
+        port: 8080,
+        host: true,
+        strictPort: true
+    },
+    server: {
+        port: 8080,
+        strictPort: true,
+        host: true,
+        origin: 'http://0.0.0.0:8080'
+    }
 })
